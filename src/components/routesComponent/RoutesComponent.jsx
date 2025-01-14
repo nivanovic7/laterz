236cd4fc4078bbd8eda05acf7d995d0f4a1b59d1
Nikola Ivanovic <ivanovic.nikola.ns@gmail.com>
2025-01-14 12:24:10 +0100

    Added styles to sidevar

--- src/components/routesComponent/RoutesComponent.jsx
+++ src/components/routesComponent/RoutesComponent.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Socialize from "../../pages/socialize/Socialize";
 import Outfits from "../../pages/outfits/Outfits";
 import LocalHub from "../../pages/localHub/LocalHub";
@@ -10,9 +10,10 @@ class RoutesComponent extends React.Component {
     return (
       <Routes>
         <Route path="/" element={<Socialize />} />
+        {/* <Route path="/socialise" element={<Navigate to="/" replace />} /> */}
         <Route path="/outfits" element={<Outfits />} />
-        <Route path="/local-hub" element={<LocalHub />} />
-        <Route path="/shout-outs" element={<ShoutOuts />} />
+        <Route path="/localhub" element={<LocalHub />} />
+        <Route path="/shoutouts" element={<ShoutOuts />} />
       </Routes>
     );
   }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Sidebar.module.css";
+import { NavLink } from "react-router-dom";
 
 class Sidebar extends React.Component {
   render() {
@@ -7,22 +8,30 @@ class Sidebar extends React.Component {
       <aside
         className={`${styles.aside} d-flex-col gap-20 b-radius-primary bg-neutral px-m py-m`}
       >
-        <div className={`${styles.socialise} fs-xl fw-medium clr-neutral`}>
-          Socialise
-        </div>
-        <div
-          className={`${styles.shoutouts} fs-xl fw-medium clr-neutral b-radius-tertiary`}
-        >
-          <span>Shout</span>
-          <span className={`fs-l`}>Outs</span>
-        </div>
-        <div className={`${styles.localhub} fs-xl fw-medium clr-neutral`}>
-          <span>Local</span>
-          <span className={`fs-l`}>Hub</span>
-        </div>
-        <div className={`${styles.outfits} fs-xl fw-medium clr-neutral`}>
-          Outfits
-        </div>
+        <NavLink to="/">
+          <div className={`${styles.socialise} fs-xl fw-medium clr-neutral`}>
+            Socialise
+          </div>
+        </NavLink>
+        <NavLink to="shoutouts">
+          <div
+            className={`${styles.shoutouts} fs-xl fw-medium clr-neutral b-radius-tertiary`}
+          >
+            <span>Shout</span>
+            <span className={`fs-l`}>Outs</span>
+          </div>
+        </NavLink>
+        <NavLink to="localhub">
+          <div className={`${styles.localhub} fs-xl fw-medium clr-neutral`}>
+            <span>Local</span>
+            <span className={`fs-l`}>Hub</span>
+          </div>
+        </NavLink>
+        <NavLink to="outfits">
+          <div className={`${styles.outfits} fs-xl fw-medium clr-neutral`}>
+            Outfits
+          </div>
+        </NavLink>
       </aside>
     );
   }
